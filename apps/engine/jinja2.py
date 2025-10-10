39baa6fc0d50995364eaeb45427df3084c6a0edf
CongPPT <congppt2002@gmail.com>
2025-10-10 13:03:55 +0700

    simple modal

--- apps/engine/jinja2.py
+++ apps/engine/jinja2.py
@@ -4,29 +4,39 @@ from django.urls import reverse
 from jinja2 import Environment
 from sample.templatetags.table_filters import get_badge_color
 from datetime import date
-
+from pydantic_core import to_jsonable_python
 
 def environment(**options):
     env = Environment(**options)
-    env.globals.update({
-        'static': staticfiles_storage.url,
-        'url': reverse,
-    })
+    env.globals.update(
+        {
+            "static": staticfiles_storage.url,
+            "url": reverse,
+        }
+    )
+
     def get_attribute(object, key):
         """Get item from dictionary by key"""
         if isinstance(object, dict):
-            return object.get(key, '-')
-        return getattr(object, key, '-')
+            return object.get(key, "-")
+        return getattr(object, key, "-")
+
     # Add custom filters
-    env.filters['get_attribute'] = get_attribute
-    env.filters['get_badge_color'] = get_badge_color
-    
+    env.filters["get_attribute"] = get_attribute
+    env.filters["get_badge_color"] = get_badge_color
+
     # Add date formatting filter
     def strftime(value, format_string):
         if isinstance(value, date):
             return value.strftime(format_string)
         return value or "-"
-    
-    env.filters['strftime'] = strftime
-    
+
+    env.filters["strftime"] = strftime
+
+    # Override tojson filter
+    def tojson(value):
+        return json.dumps(to_jsonable_python(value))
+
+    env.filters["tojson"] = tojson
+
     return env
