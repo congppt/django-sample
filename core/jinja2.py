41e3329ca20b3e942bd2a1d7fe18c05311832a90
CongPPT <congppt2002@gmail.com>
2026-03-10 19:33:24 +0700

    reinitialize

---
+++ core/jinja2.py
@@ -0,0 +1,38 @@
+import json
+from datetime import date, datetime
+
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.urls import reverse
+from django.utils import translation
+from django.middleware.csrf import get_token
+from jinja2 import Environment
+from pydantic_core import to_jsonable_python
+
+
+
+def environment(**options):
+    env = Environment(extensions=['jinja2.ext.i18n'], **options)
+    env.install_gettext_translations(translation)
+    
+    env.globals.update(
+        {
+            "static": staticfiles_storage.url,
+            "reverse": reverse,
+        }
+    )
+
+    def get_attribute(object, key, default=None):
+        """Get item from dictionary by key"""
+        if isinstance(object, dict):
+            return object.get(key, default)
+        return getattr(object, key, default)
+
+    # 
+    env.filters["get_attribute"] = get_attribute
+
+    def jsonify(value):
+        return json.dumps(to_jsonable_python(value))
+
+    env.filters["jsonify"] = jsonify
+
+    return env
---
+++ env.py
@@ -0,0 +1,48 @@
+import os
+from dotenv import load_dotenv
+from urllib.parse import urlparse
+import pathlib
+
+load_dotenv()
+
+__ENV = os.getenv('ENV', 'LOCAL').upper()
+IS_PRODUCTION = __ENV == "PRODUCTION"
+IS_LOCAL = __ENV == "LOCAL"
+
+DB_URL = os.getenv('DB_URL')
+if not DB_URL:
+    raise EnvironmentError("DB_URL is not configured")
+__parsed_db_url = urlparse(DB_URL)
+DB_HOST = __parsed_db_url.hostname
+DB_PORT = __parsed_db_url.port
+DB_USER = __parsed_db_url.username
+DB_PASSWORD = __parsed_db_url.password
+DB_NAME = __parsed_db_url.path.lstrip('/')
+
+LOG_DIR = os.getenv('LOG_DIR', 'logs')
+
+SECRET_KEY = os.getenv('SECRET_KEY')
+if not SECRET_KEY:
+    raise EnvironmentError("SECRET_KEY is not configured")
+
+HOST_URL = os.getenv('HOST_URL', 'http://localhost:8000')
+if not HOST_URL:
+    raise EnvironmentError("HOST_URL is not configured")
+
+__exclude_dirs = ['core', 'static', 'staticfiles', 'utils', LOG_DIR ]
+
+# 2. Get the directory where the current script is located
+__current_dir = pathlib.Path(__file__).parent.resolve()
+
+def __get_app_name(base_path: pathlib.Path, ignore_list: list[str]) -> str | None:
+    # Iterate through all items in the current directory
+    for path in base_path.iterdir():
+        # Check if it's a directory AND it's not a hidden directory and its name isn't in your list
+        if path.is_dir() and not path.name.startswith('.') and path.name not in ignore_list:
+            return path.name
+    raise SystemError("No app name found")
+
+# Execute if not hardcoded
+APP_NAME = "" or __get_app_name(__current_dir, __exclude_dirs)
+if not APP_NAME:
+    raise EnvironmentError("No app name found")
