81ca22785bdf338b7e52b1f1609673e8288d272c
CongPPT <congppt2002@gmail.com>
2026-03-16 12:01:29 +0700

    fix: button

--- core/jinja2.py
+++ core/jinja2.py
@@ -1,38 +1,34 @@
 import json
-from datetime import date, datetime
 
 from django.contrib.staticfiles.storage import staticfiles_storage
 from django.urls import reverse
-from django.utils import translation
 from django.middleware.csrf import get_token
 from jinja2 import Environment
 from pydantic_core import to_jsonable_python
 
+from utils.mock import number
 
 
 def environment(**options):
-    env = Environment(extensions=['jinja2.ext.i18n'], **options)
-    env.install_gettext_translations(translation)
-    
-    env.globals.update(
-        {
-            "static": staticfiles_storage.url,
-            "url": reverse,
-        }
-    )
-
+    env = Environment(**options)
+    def jsonify(value):
+        return json.dumps(to_jsonable_python(value))
     def get_attribute(object, key, default=None):
         """Get item from dictionary by key"""
         if isinstance(object, dict):
             return object.get(key, default)
         return getattr(object, key, default)
+    def random_id(prefix: str):
+        return f"{prefix}-{number(1000, 9999)}"
 
-    # 
-    env.filters["get_attribute"] = get_attribute
-
-    def jsonify(value):
-        return json.dumps(to_jsonable_python(value))
-
-    env.filters["jsonify"] = jsonify
+    env.globals.update(
+        {
+            "static": staticfiles_storage.url,
+            "url": reverse,
+            "jsonify": jsonify,
+            "get_attribute": get_attribute,
+            "random_id": random_id,
+        }
+    )
 
     return env
